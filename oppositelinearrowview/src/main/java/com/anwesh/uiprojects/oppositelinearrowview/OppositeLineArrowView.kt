07aaf959f1683cf93cb3df8bdb918820e3b1cbba
Anwesh43 <anweshthecool0@gmail.com>
2019-06-03 02:56:59 +0530

    adding OppositeLineArrowView class

--- oppositelinearrowview/src/main/java/com/anwesh/uiprojects/oppositelinearrowview/OppositeLineArrowView.kt
+++ oppositelinearrowview/src/main/java/com/anwesh/uiprojects/oppositelinearrowview/OppositeLineArrowView.kt
@@ -81,3 +81,21 @@ fun Canvas.drawOLANode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class OppositeLineArrowView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
