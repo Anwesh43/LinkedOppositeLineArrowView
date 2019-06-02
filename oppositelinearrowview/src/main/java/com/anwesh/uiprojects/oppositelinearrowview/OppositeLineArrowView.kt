13d484075129ba16d5008be124144278af953510
Anwesh43 <anweshthecool0@gmail.com>
2019-06-03 02:55:25 +0530

    adding Canvas extesnion methods

--- oppositelinearrowview/src/main/java/com/anwesh/uiprojects/oppositelinearrowview/OppositeLineArrowView.kt
+++ oppositelinearrowview/src/main/java/com/anwesh/uiprojects/oppositelinearrowview/OppositeLineArrowView.kt
@@ -21,6 +21,9 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#00838F")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val arrowFactor : Float = 3.6f
+val arrowRotDeg : Float = 45f
+val rFactor : Float = 3.1f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
@@ -31,3 +34,50 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawArrow(size : Float, paint : Paint) {
+    for (j in 0..1) {
+        save()
+        rotate(45f * (1 - 2 * j))
+        drawLine(0f, 0f, -size / arrowFactor, 0f, paint)
+        restore()
+    }
+}
+
+fun Canvas.drawLineArrowArc(i : Int, w : Float, size : Float, sc1 : Float, sc2 : Float, paint : Paint) {
+    val r : Float = size / rFactor
+    val sc1i : Float = sc1.divideScale(i, lines)
+    val sc2i : Float = sc2.divideScale(i, lines)
+    val x : Float = w * sc1i
+    save()
+    scale(1f - 2 * i, 1f - 2 * i)
+    save()
+    translate(-w, -r)
+    drawLine(0f, 0f, x, 0f, paint)
+    drawArc(RectF(-r, 0f, r, 2 * r), -90f, 180f * sc2i, false, paint)
+    restore()
+    save()
+    rotate(180f * sc2)
+    translate(-w + x, -r)
+    drawArrow(size, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawOLANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w / 2, gap * (i + 1))
+    for (j in 0..(lines - 1)) {
+        drawLineArrowArc(j, w / 2, size, sc1, sc2, paint)
+    }
+    restore()
+}
