19bf7e418ec23cbaa823ef99dafae5832ac00d47
Anwesh43 <anweshthecool0@gmail.com>
2019-06-03 02:39:50 +0530

    adding constants and libraries

--- oppositelinearrowview/src/main/java/com/anwesh/uiprojects/oppositelinearrowview/OppositeLineArrowView.kt
+++ oppositelinearrowview/src/main/java/com/anwesh/uiprojects/oppositelinearrowview/OppositeLineArrowView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.oppositelinearrowview
 /**
  * Created by anweshmishra on 03/06/19.
  */
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#00838F")
+val backColor : Int = Color.parseColor("#BDBDBD")
