5e111f5262cdfbe0a822a00fedcfe4637d870da0
Anwesh43 <anweshthecool0@gmail.com>
2019-06-03 03:10:57 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedoppositelinearrowview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedoppositelinearrowview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedoppositelinearrowview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.oppositelinearrowview.OppositeLineArrowView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        OppositeLineArrowView.create(this)
     }
 }
